45fbf155606ada399fb7c5d0703c6ad10d475198
Janelynn Camingue <greentomboy@sbcglobal.net>
2019-08-10 13:35:00 -0700

    Updates

---
+++ precache-manifest.9066ce1616a932334b018bc00635ee94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa7076de9f205b2584810db059aaaae",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "6c39e1c99e07e26fd39b",
+    "url": "/Portfolio-Website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "def0d2186da3f9a8f0d2",
+    "url": "/Portfolio-Website/static/js/2.e0a3501e.chunk.js"
+  },
+  {
+    "revision": "6c39e1c99e07e26fd39b",
+    "url": "/Portfolio-Website/static/js/main.360a1bc9.chunk.js"
+  },
+  {
+    "revision": "3912871282a8e65fc7ec",
+    "url": "/Portfolio-Website/static/js/runtime~main.101425da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio-Website/static/media/logo.5d5d9eef.svg"
+  }
+]);
