63ce7317f1aabf2ac182fcdbe53f6dedf6f04f2b
Janelynn Camingue <greentomboy@sbcglobal.net>
2019-11-07 11:03:14 -0800

    Updates

---
+++ precache-manifest.77e7bc03733fb001473e5d2d5b5b352b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bc4786e209204c69a0b6b71a47287d",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "b19b32b4a6b7a56d30ab",
+    "url": "/Portfolio-Website/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3b04dee0c766441d75de",
+    "url": "/Portfolio-Website/static/css/main.b3267fe7.chunk.css"
+  },
+  {
+    "revision": "b19b32b4a6b7a56d30ab",
+    "url": "/Portfolio-Website/static/js/2.305bdb8f.chunk.js"
+  },
+  {
+    "revision": "3b04dee0c766441d75de",
+    "url": "/Portfolio-Website/static/js/main.54718a40.chunk.js"
+  },
+  {
+    "revision": "6a42aa3acf02d87b10f5",
+    "url": "/Portfolio-Website/static/js/runtime~main.f1a07520.js"
+  },
+  {
+    "revision": "10c87ffa4c9433be248fbd6ef61b5be7",
+    "url": "/Portfolio-Website/static/media/logo.10c87ffa.svg"
+  }
+]);
