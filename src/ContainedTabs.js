b9439a6c71661028b7394bde9429db2d860a392d
Janelynn Camingue <janelynn@janelynnc.com>
2021-06-17 22:36:35 -0700

    new resume

--- src/ContainedTabs.js
+++ src/ContainedTabs.js
@@ -99,7 +99,7 @@
         ))}
         <a class="MuiButtonBase-root MuiTab-root makeStyles-root-2 makeStyles-root-5 MuiTab-textColorInherit Mui-selected makeStyles-selected-3 makeStyles-selected-6" target="_blank" tabindex="0" role="tab" aria-disabled="false" aria-selected="true" 
         path="/" 
-        href="https://firebasestorage.googleapis.com/v0/b/portfolio-535ed.appspot.com/o/flamelink%2Fmedia%2FJanelynn%20Camingue's%20Resume_%20As%20of%209_25_20.pdf?alt=media&token=792f9bd0-9832-4584-9652-bc9a5f29823e" 
+        href="https://firebasestorage.googleapis.com/v0/b/portfolio-535ed.appspot.com/o/flamelink%2Fmedia%2FJanelynn%20Camingue's%20CV.pdf?alt=media&token=9495ba8f-c2f1-4572-8b82-cbce0a2a36c9" 
         download><span class="MuiTab-wrapper makeStyles-wrapper-4">RESUME</span><span class="MuiTouchRipple-root"></span></a>
       </Tabs>
     );
