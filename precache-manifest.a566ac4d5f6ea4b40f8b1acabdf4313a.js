5b84ecc8d683ac0f08914784d83c9c79cb05040a
Janelynn Camingue <janelynn@janelynnc.com>
2021-06-17 22:40:15 -0700

    Updates

---
+++ precache-manifest.a566ac4d5f6ea4b40f8b1acabdf4313a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7826fc92bc092c6b684568644c984179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3af7a44c21dfb77573f",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "998a6b81097841e5c880",
+    "url": "/static/css/main.c04eb50c.chunk.css"
+  },
+  {
+    "revision": "d3af7a44c21dfb77573f",
+    "url": "/static/js/2.a309d0bc.chunk.js"
+  },
+  {
+    "revision": "998a6b81097841e5c880",
+    "url": "/static/js/main.1a8db193.chunk.js"
+  },
+  {
+    "revision": "0f9571fe81602c3b51b7",
+    "url": "/static/js/runtime~main.244a79c9.js"
+  },
+  {
+    "revision": "b7e39532f0ba5970acc7ad039e8e3fd5",
+    "url": "/static/media/itchio-logo-textless-white.b7e39532.svg"
+  },
+  {
+    "revision": "b45cea166a0e3f6fae8a84422ce5bf12",
+    "url": "/static/media/logo.b45cea16.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21d820b4bb37db1852dfdd21dfaaee0b.js"
+  "/precache-manifest.a566ac4d5f6ea4b40f8b1acabdf4313a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
